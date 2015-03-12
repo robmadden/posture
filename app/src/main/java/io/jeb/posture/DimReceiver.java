a77b9331f982e0b17ac7b96a8d47a3d413f230fd
rmadden <madden.rob@gmail.com>
2015-03-11 19:17:06 -0500

    Changes

--- app/src/main/java/io/jeb/posture/DimReceiver.java
+++ app/src/main/java/io/jeb/posture/DimReceiver.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class DimReceiver extends BroadcastReceiver {
     private Activity activity;
@@ -18,7 +17,7 @@ public class DimReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         int brightness = intent.getIntExtra(DimService.INTENT_EXTRA_BRIGHTNESS, 1);
-        Log.d("BRIGHTNESS: ", String.valueOf(brightness));
+//        Log.d("BRIGHTNESS: ", String.valueOf(brightness));
         android.provider.Settings.System.putInt(context.getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS, brightness);
     }
 }
